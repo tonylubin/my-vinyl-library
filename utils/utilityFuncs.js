1c124f5541b20312e4822d89edec62f084040968
Anthony Lubin <anthonylubin@yahoo.co.uk>
2023-04-28 18:37:41 +0100

    added error function

--- utils/utilityFuncs.js
+++ utils/utilityFuncs.js
@@ -6,3 +6,6 @@ export const API_BASE = "http://localhost:3000/api/tracks";
 export const capitaliseWord = (word) => {
     return word.charAt(0).toUpperCase() + word.slice(1);
   };
+
+// to handle error serializing error DATE object
+export const serializeErrorFunc = (data) => JSON.parse(JSON.stringify(data));  
