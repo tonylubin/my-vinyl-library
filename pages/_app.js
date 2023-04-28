69210897fefb8559e614596516549e1fd0837025
Anthony Lubin <anthonylubin@yahoo.co.uk>
2023-04-28 18:37:15 +0100

    added context

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,48 @@
 import "@/styles/globals.css";
-import { BioRhyme } from 'next/font/google';
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import { BioRhyme } from "next/font/google";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import Title from "@/components/Head";
+import { createContext, state, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 config.autoAddCss = false;
 
-
-const bioRhyme = BioRhyme({ 
+// google font
+const bioRhyme = BioRhyme({
   weight: ["200", "400", "700", "800"],
-  subsets: ['latin'],
+  subsets: ["latin"],
   style: ["normal"],
   variable: "--font-bioRhyme",
- });
+});
 
+
+export const UserContext = createContext();
 
 export default function App({ Component, pageProps }) {
+  
+  const [ formData, setFormData ] = useState();
+
   return (
-    <div className={`h-full w-full overflow-hidden text-primary-textColour ${bioRhyme.variable}`}>
-      <Title />
-      <Component {...pageProps} />
-    </div>
-  )
-};
+    <UserContext.Provider value={{setFormData, formData}}>
+      <div
+        className={`h-full w-full overflow-hidden text-primary-textColour ${bioRhyme.variable}`}
+      >
+        <Title />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+        <Component {...pageProps} />
+      </div>
+    </UserContext.Provider>
+  );
+}
